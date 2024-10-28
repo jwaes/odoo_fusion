e672b5acdc21bf0d5d9f1e1a6a83dda30b8542c0
jaco <jaco.waes@gmail.com>
2024-10-28 17:40:37 +0100

    [FIX] fusion_addon: fix last_updated_by relationship (v17.0.1.0.11)

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Odoo Fusion Integration',
-    'version': '17.0.1.0.8',
+    'version': '17.0.1.0.11',
     'category': 'Manufacturing',
     'summary': 'Integration between Odoo and Autodesk Fusion 360',
     'author': 'Jaco Tech',
