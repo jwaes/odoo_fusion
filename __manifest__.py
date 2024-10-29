491ec0394c1e9cf24a00c0087eb1a1ffdfa34bea
jaco <jaco.waes@gmail.com>
2024-10-29 16:10:25 +0100

    f

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Odoo Fusion Integration',
-    'version': '17.0.1.0.39',
+    'version': '17.0.1.0.40',
     'category': 'Manufacturing',
     'summary': 'Integration between Odoo and Autodesk Fusion 360',
     'author': 'Jaco Tech',
