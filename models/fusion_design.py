765c2c65892e8a05b7829751b1396152654590a0
jaco <jaco.waes@gmail.com>
2024-10-29 08:39:02 +0100

    [IMP] fusion_addon: refactor fusion.design model (v17.0.1.0.14)
    Add sync and sharing fields to fusion.design
    Extract common code into _prepare_fusion_vals method
    Remove duplicate code in create/write operations
    Add state and last_sync tracking

--- models/fusion_design.py
+++ models/fusion_design.py
@@ -13,11 +13,43 @@ class FusionDesign(models.Model):
     version_number = fields.Integer('Version Number')
     last_updated_by_id = fields.Many2one('fusion.user', 'Last Updated By')
     fusion_web_url = fields.Char('Fusion Web URL')
-    shared_link = fields.Many2one('fusion.shared.link', 'Shared Link')
     date_created = fields.Datetime('Date Created')
     date_modified = fields.Datetime('Date Modified')
     product_ids = fields.One2many('product.template', 'fusion_design_id', string='Fusion Products')
     default_code = fields.Char('Reference', readonly=True)
+    
+    # Sync fields
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('synced', 'Synced'),
+        ('error', 'Error')
+    ], string='Status', default='draft', required=True)
+    last_sync = fields.Datetime('Last Sync')
+    
+    # Sharing fields
+    shared_link_active = fields.Boolean('Shared', default=False)
+    shared_link_download = fields.Boolean('Allow Download', default=False)
+    shared_link_url = fields.Char('Share URL')
+
+    def _prepare_fusion_vals(self, fusion_data):
+        """Prepare values for create/write from fusion data."""
+        return {
+            'name': fusion_data['name'],
+            'fusion_uuid': fusion_data['fusion_uuid'],
+            'version': fusion_data['version'],
+            'description': fusion_data['description'],
+            'version_id': fusion_data['version_id'],
+            'version_number': fusion_data['version_number'],
+            'last_updated_by_id': self.env['fusion.user'].search([('fusion_id', '=', fusion_data['last_updated_by']['fusion_id'])], limit=1).id,
+            'fusion_web_url': fusion_data['fusion_web_url'],
+            'date_created': fusion_data['date_created'],
+            'date_modified': fusion_data['date_modified'],
+            'shared_link_active': fusion_data['shared_link']['is_shared'],
+            'shared_link_download': fusion_data['shared_link']['allow_download'],
+            'shared_link_url': fusion_data['shared_link']['url'],
+            'state': 'synced',
+            'last_sync': fields.Datetime.now(),
+        }
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -36,34 +68,12 @@ class FusionDesign(models.Model):
     def create_or_update_from_fusion(self, fusion_data):
         """Create or update Odoo fusion design from Fusion data."""
         design = self.search([('fusion_uuid', '=', fusion_data['fusion_uuid'])], limit=1)
+        vals = self._prepare_fusion_vals(fusion_data)
+        
         if not design:
-            design = self.create({
-                'name': fusion_data['name'],
-                'fusion_uuid': fusion_data['fusion_uuid'],
-                'version': fusion_data['version'],
-                'description': fusion_data['description'],
-                'version_id': fusion_data['version_id'],
-                'version_number': fusion_data['version_number'],
-                'last_updated_by_id': self.env['fusion.user'].search([('fusion_id', '=', fusion_data['last_updated_by']['fusion_id'])], limit=1).id,
-                'fusion_web_url': fusion_data['fusion_web_url'],
-                'shared_link': self.env['fusion.shared.link'].create_or_update_from_fusion(fusion_data['shared_link']).id,
-                'date_created': fusion_data['date_created'],
-                'date_modified': fusion_data['date_modified'],
-            })
+            design = self.create(vals)
         else:
-            design.write({
-                'name': fusion_data['name'],
-                'fusion_uuid': fusion_data['fusion_uuid'],
-                'version': fusion_data['version'],
-                'description': fusion_data['description'],
-                'version_id': fusion_data['version_id'],
-                'version_number': fusion_data['version_number'],
-                'last_updated_by_id': self.env['fusion.user'].search([('fusion_id', '=', fusion_data['last_updated_by']['fusion_id'])], limit=1).id,
-                'fusion_web_url': fusion_data['fusion_web_url'],
-                'shared_link': self.env['fusion.shared.link'].create_or_update_from_fusion(fusion_data['shared_link']).id,
-                'date_created': fusion_data['date_created'],
-                'date_modified': fusion_data['date_modified'],
-            })
+            design.write(vals)
         
         # Create or update Fusion Products
         for component in fusion_data['components']:
